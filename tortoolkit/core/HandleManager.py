73ed146b098ead10bf110753633d6f4eb6ac12d0
diviiac <93221743+diviiac@users.noreply.github.com>
2021-10-27 02:16:12 +0530

    Update HandleManager.py

--- tortoolkit/core/HandleManager.py
+++ tortoolkit/core/HandleManager.py
@@ -644,7 +644,7 @@ async def set_password_zip(message):
 
 
 async def start_handler(event):
-    msg = "Hello This is TorToolkitX running on heroku an instance of <a href='https://github.com/XcodersHub/TorToolkitX'>This Repo</a>. Try the repo for yourself and dont forget to put a STAR and fork."
+    msg = "Hello, this bot is powered by the owners of <a href='https://t.me/indiananimeclub'>this group</a>. Join the group for more details."
     await event.reply(msg, parse_mode="html")
 
 
